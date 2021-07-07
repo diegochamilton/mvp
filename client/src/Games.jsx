4334e98a20fc0abe4737d7ee5fd2f2ae71d3d076
Diego Coronel <diegocoronel1997@gmail.com>
2021-07-07 15:36:08 -0400

    API set

---
+++ client/src/Games.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Games = () => {
+  return (
+    <div>
+      Hello World
+    </div>
+  );
+}
+
+export default Games;
--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Games from "./Games.jsx";
+import axios from "axios";
+import KEYS from "../../config.js";
 
 const App = () => {
-  return (<div>
-    Hello World!
-  </div>)
+  axios({
+    method: 'get',
+    url: `${KEYS.ENDPOINT}/TeamSeasonStats/2021?key=${KEYS.API_KEY}`,
+  })
+  .then(res => console.log(res.data))
+  .catch(err => console.log(err))
+
+  return (
+    <div>
+      <Games />
+    </div>
+  )
 }
 
 export default App;
