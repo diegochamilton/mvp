b4d8a8aaf0a1c0e15ecfa6e4b1840f42be047239
Diego Coronel <diegocoronel1997@gmail.com>
2021-07-07 14:52:51 -0400

    Webpack set

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,20 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
   resolve: {
     extensions: ["*", ".js", ".jsx"],
   },
