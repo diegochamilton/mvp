66737c53740f01c2d208f45105a42dbfa0930559
Diego Coronel <diegocoronel1997@gmail.com>
2021-07-08 22:07:06 -0400

    PORT config

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const parser = require("body-parser");
 const { getStats, getTeams } = require("./helpers/apiHelpers.js");
 // const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 const mockData = require("../mockData.js");
 const { getMatchResults } = require("./helpers/statsGenerator.js");
 
